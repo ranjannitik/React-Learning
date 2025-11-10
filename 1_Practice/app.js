31db92d5af37137c815830646793d8f932ea7eb1
Nitik Ranjan <ranjannitik0611@gmail.com>
2025-11-10 13:38:20 +0530

    1st Day

---
+++ 1_Practice/app.js
@@ -0,0 +1,11 @@
+//1.Default Export
+
+//    const a = 10;
+//    export default a;
+
+
+//Named Export
+
+   export var arr=[1,2,3,4];
+   var name ="Nitik";
+   export default name;
---
+++ 1_Practice/script.js
@@ -0,0 +1,21 @@
+// var v1=document.createElement('h1');
+// v1.innerHTML='Hello from JS';
+// document.body.appendChild(v1); 
+
+
+
+//1.Default import
+
+
+  // import a from './app.js';
+  // console.log(a);
+ 
+
+//2.Named Import
+    
+    import NaamBatao from './app.js'
+    console.log(NaamBatao);
+
+    import{arr}from './app.js'
+    console.log(arr);
+    
